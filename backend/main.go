6661f3bded96c5c5253fd4d9562be64438efecd8
dana-futa <128000092+dana-futa@users.noreply.github.com>
2023-11-26 20:52:02 -0700

    DF - fixed row mapper/scanner and deleted old files

--- backend/main.go
+++ backend/main.go
@@ -7,27 +7,26 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
-	// _ "github.com/mattn/go-sqlite3"
 	_ "modernc.org/sqlite"
 )
 
 var DB *sql.DB
 
 type Employee struct {
-	EmployeeId int    `json:"employeeId"` // EmployeeId starts with uppercase to make it an exported field
+	EmployeeID int    `json:"employeeId"` // EmployeeID starts with uppercase to make it an exported field
 	FirstName  string `json:"firstName"`  // In json format, use proper camel case and convert between the 2 field formats
 	LastName   string `json:"lastName"`
 	Email      string `json:"email"`
 	Title      string `json:"title"`
-	ManagerId  int    `json:"managerId"`
+	ManagerID  int    `json:"managerId"` // can be nil, default to -1
 	IsActive   bool   `json:"isActive"`
 }
 
-var tempEmployees = []Employee{
-	{EmployeeId: 0, FirstName: "Bob", LastName: "Stevens", Email: "bob.stevens@business.com", Title: "CEO", IsActive: true}, // ManagerId: nil (Go will set this as default)
-	{EmployeeId: 1, FirstName: "Mary", LastName: "Jane", Email: "mary.jane@business.com", Title: "Engineer 1", ManagerId: 0, IsActive: true},
-	{EmployeeId: 2, FirstName: "John", LastName: "Hope", Email: "john.hope@business.com", Title: "Engineer 1", ManagerId: 0, IsActive: true},
-	{EmployeeId: 2, FirstName: "David", LastName: "Gomez", Email: "david.gomez@business.com", Title: "Engineer 2", ManagerId: 1, IsActive: true},
+var testEmployees = []Employee{
+	{EmployeeID: 0, FirstName: "Bob", LastName: "Stevens", Email: "bob.stevens@business.com", Title: "CEO", IsActive: true}, // ManagerId: nil (Go will set this as default)
+	{EmployeeID: 1, FirstName: "Mary", LastName: "Jane", Email: "mary.jane@business.com", Title: "Engineer 1", ManagerID: 0, IsActive: true},
+	{EmployeeID: 2, FirstName: "John", LastName: "Hope", Email: "john.hope@business.com", Title: "Engineer 1", ManagerID: 0, IsActive: true},
+	{EmployeeID: 3, FirstName: "David", LastName: "Gomez", Email: "david.gomez@business.com", Title: "Engineer 2", ManagerID: 1, IsActive: true},
 }
 
 func checkErr(err error) {
@@ -36,28 +35,26 @@ func checkErr(err error) {
 	}
 }
 
-// func CreateDatabaseConnection() error {
-// 	db, err := sql.Open("sqlite3", "business.db")
-// 	if err != nil {
-// 		return err
-// 	}
+func CreateDatabaseConnection() error {
+	db, err := sql.Open("sqlite", "../database/business.db")
+	if err != nil {
+		fmt.Println("An error in the db connection has occured.")
+		return err
+	}
+	DB = db
 
-// 	DB = db
+	fmt.Println("Database connection successful!")
 
-// 	fmt.Println("Database connection successful!")
-
-// 	defer DB.Close()
-
-// 	return nil
-// }
+	return nil
+}
 
 /**
-* Practice function to return tempEmployees as a raw struct (dynamic array) to get the API working.
+* Practice function to return testEmployees as a raw struct (dynamic array) to get the API working.
 * @param context - information about the request
-* @return tempEmployees - slice
+* @return testEmployees - slice
  */
-func GetTempEmployees(context *gin.Context) {
-	context.IndentedJSON(http.StatusOK, tempEmployees)
+func GetTestEmployees(context *gin.Context) {
+	context.IndentedJSON(http.StatusOK, gin.H{"data": testEmployees})
 }
 
 func GetAllActiveEmployees(context *gin.Context) {
@@ -65,10 +62,10 @@ func GetAllActiveEmployees(context *gin.Context) {
 	checkErr(err)
 
 	if employees == nil {
-		context.JSON(http.StatusBadRequest, gin.H{"error": "No Records Found"})
+		context.IndentedJSON(http.StatusBadRequest, gin.H{"error": "No Records Found."})
 		return
 	} else {
-		context.JSON(http.StatusOK, gin.H{"data": employees})
+		context.IndentedJSON(http.StatusOK, gin.H{"data": employees})
 	}
 }
 
@@ -93,16 +90,24 @@ func GetAllActiveEmployeesHelper() ([]Employee, error) {
 	// iterate through rows of data retrieved from the database
 	for rows.Next() {
 		emp := Employee{}
+		var managerID sql.NullInt64
 
 		// bind the values in the row to the emp object
-		err = rows.Scan(&emp.EmployeeId, &emp.FirstName, &emp.LastName, &emp.Email, &emp.Title, &emp.ManagerId, &emp.IsActive)
+		err = rows.Scan(&emp.EmployeeID, &emp.FirstName, &emp.LastName, &emp.Email, &emp.Title, &managerID, &emp.IsActive)
 
 		// if there was an error binding the row data to the emp object, return nil for Employee struct and the error
 		if err != nil {
 			return nil, err
 		}
 
-		// add extracted/mapped emp to employees struct
+		// set managerId on emp struct
+		if managerID.Valid {
+			emp.ManagerID = int(managerID.Int64)
+		} else {
+			emp.ManagerID = -1 // default of -1 to indicate no manager
+		}
+
+		// add extracted/mapped emp to employees slice
 		employees = append(employees, emp)
 	}
 
@@ -112,24 +117,17 @@ func GetAllActiveEmployeesHelper() ([]Employee, error) {
 func main() {
 	fmt.Println("Hello world")
 
-	db, err := sql.Open("sqlite", "business.db")
-	if err != nil {
-		fmt.Println("An error in the db connection has occured")
-		return
-	}
-	DB = db
+	// connect to the database and check if there was an error when connecting
+	err := CreateDatabaseConnection()
+	checkErr(err)
 
 	// handles different routes/endpoints of the api
 	router := gin.Default()
 
-	// // connect to the database and check if there was an error when connecting
-	// err := CreateDatabaseConnection()
-	// checkErr(err)
-
 	// define API v1
 	v1 := router.Group("/api/v1")
 	{
-		v1.GET("/temp-employees", GetTempEmployees)
+		v1.GET("/test-employees", GetTestEmployees)
 		v1.GET("/all-active-employees", GetAllActiveEmployees)
 	}
 
